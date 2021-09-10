e23fa8b3efc501fb38a66484063f8b1f8b987c1e
unknown <21511777@ynu.kr>
2021-09-10 17:16:33 +0900

    2021-09-10-v1-Initial commit

---
+++ src/main/java/Influencer/Influencerspring/InfluencerSpringApplication.java
@@ -0,0 +1,13 @@
+package Influencer.Influencerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluencerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluencerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Influencer/Influencerspring/InfluencerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Influencer.Influencerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluencerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
