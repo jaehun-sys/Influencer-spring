a49791dd03796c5cc1abed16200581285d4dd288
jaehun-sys <us03098@gmail.com>
2021-09-11 03:17:31 +0900

    2021-09-11-v1

---
+++ src/main/java/Influencer/Influencerspring/controller/InfluencerController.java
@@ -0,0 +1,17 @@
+package Influencer.Influencerspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InfluencerController {
+
+    @GetMapping("influencer") //웹 어플리케이션에서 "/influencer"라고 들어오면 influencer 메소드를 호출
+    public String influencer(Model model){
+        //data를 influencer로 넘기겠다는 뜻(key는 data,  value는 influencer)
+        model.addAttribute("data", "influencer!!");
+        //templates에 있는 influencer.html로 data 가지고 넘어가라.
+        return "influencer";
+    }
+}
