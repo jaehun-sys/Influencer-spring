9870a792867cd3af8a459e58fcdca78b2d68aaf2
jaehun-sys <us03098@gmail.com>
2021-09-13 17:57:50 +0900

    2021-09-13-v2 1. API 연습

--- src/main/java/Influencer/Influencerspring/controller/InfluencerController.java
+++ src/main/java/Influencer/Influencerspring/controller/InfluencerController.java
@@ -1,5 +1,6 @@
 package Influencer.Influencerspring.controller;
 
+import org.apache.tomcat.util.net.TLSClientHelloExtractor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,13 +26,35 @@ public class InfluencerController {
         return "sample-template";
     }
 
-    //API
+    //API 연습1
     //template 엔진과의 차이 : view 가 없다.
     @GetMapping("hello-string")
-    //html에 나오는 body가 아님. http에서 head부와 body부가 있는데 그 body부에 name을 직접 넣어주겠다는 뜻
+    //html에 나오는 body가 아님. http에서 header부와 body부가 있는데 그 body부에 name을 직접 넣어주겠다는 뜻
+    //view에 주고 그런거 없이 문자 그대로 감.
     @ResponseBody
     public String helloString(@RequestParam("name") String name){
-        return "hello"; // hello spring
+        return "hello " + name; //-> "hello spring"
+    }
+    
+    //API 연습2
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;   //json 형식으로 들어감.
+    }
+
+    static class Hello{
+        private String name;    //key가 name
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
