ef1f7bf69dafe68e1d7620359a4acb54cc3c6907
jaehun-sys <us03098@gmail.com>
2021-09-13 17:22:36 +0900

    2021-09-13-v1 1. spring-boot-devtools 모듈 추가 (html 파일을 컴파일만 해주면(ctrl + shift + F9) 서버 재시작 없이 view 파일 변경 가능)

--- src/main/java/Influencer/Influencerspring/controller/InfluencerController.java
+++ src/main/java/Influencer/Influencerspring/controller/InfluencerController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class InfluencerController {
@@ -16,11 +17,21 @@ public class InfluencerController {
         return "influencer";
     }
 
+    //MVC template 엔진
     //외부에서 데이터 입력
-    @GetMapping("mvc-test")
+    @GetMapping("hello-mvc")
     public String mvcTest(@RequestParam(value = "name", required = true) String name, Model model){
         model.addAttribute("name", name);
-        return "test-template";
+        return "sample-template";
+    }
+
+    //API
+    //template 엔진과의 차이 : view 가 없다.
+    @GetMapping("hello-string")
+    //html에 나오는 body가 아님. http에서 head부와 body부가 있는데 그 body부에 name을 직접 넣어주겠다는 뜻
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"; // hello spring
     }
 
 }
