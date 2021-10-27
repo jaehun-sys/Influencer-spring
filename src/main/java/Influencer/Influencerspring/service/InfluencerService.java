5c106bbbaa2e2eda916449423d8841557b4521fe
jaehun-sys <us03098@gmail.com>
2021-10-27 23:21:00 +0900

    2021-10-27-v1 1. Spring Data Jpa 공부 시작. -> 페이징

--- src/main/java/Influencer/Influencerspring/service/InfluencerService.java
+++ src/main/java/Influencer/Influencerspring/service/InfluencerService.java
@@ -5,7 +5,12 @@ import Influencer.Influencerspring.domain.InfProfile;
 
 import Influencer.Influencerspring.repository.HashtagRepository;
 import Influencer.Influencerspring.repository.InfluencerRepository;
+import Influencer.Influencerspring.repository.JpaInfluencerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -29,5 +34,11 @@ public class InfluencerService {
         return influencerRepository.findByInf_username(inf_username);
     }
 
+    /* 페이징 */
+/*    public Page<InfProfile> getCompanyPage(Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "id"));
+        return influencerRepository.findAll(pageable);
+    }*/
 
 }
